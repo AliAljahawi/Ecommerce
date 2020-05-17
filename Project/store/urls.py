b13a8333babbe520a5b46f7f3ea78cbc6d189e20
AliAljahawi <63263630+AliAljahawi@users.noreply.github.com>
2020-05-17 09:35:36 +0200

    templatesadjustment

--- Project/store/urls.py
+++ Project/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     
     path('', views.home, name="Store Home"),
-    path('listview', views.store, name="store"),
+    path('listview', views.store, name="listview"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('detailview', views.detailview, name="detailview"),
