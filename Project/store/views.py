c513500653a9bb6b5a34e4ef3001e9e8ae848031
FreeDom\FreeDomLy <absy.bo32@gmail.com>
2020-05-17 23:12:10 +0200

    created cart view

--- Project/store/views.py
+++ Project/store/views.py
@@ -12,8 +12,13 @@ def store(request):
     return render(request, 'store/listview.html', context)
 
 def cart(request):
-    #for me absy
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items':items , 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
