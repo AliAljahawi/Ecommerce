c751509e7c3354fd33c36ec024607f0aabd039c6
AliAljahawi <63263630+AliAljahawi@users.noreply.github.com>
2020-05-20 07:17:50 +0200

    checks

--- Project/store/views.py
+++ Project/store/views.py
@@ -57,7 +57,7 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request,username=username, password=username)
+        user = authenticate(request,username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('Store Home')
